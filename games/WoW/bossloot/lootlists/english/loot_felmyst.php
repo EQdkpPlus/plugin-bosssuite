0cc7e1b181dff9dd115482a7de4182bafa725479
sz3 <sz3@eqdkp-plus.eu>
2008-02-21 01:17:34 +0000

    updatet lootlists

--- games/WoW/bossloot/lootlists/english/loot_felmyst.php
+++ games/WoW/bossloot/lootlists/english/loot_felmyst.php
@@ -4,7 +4,7 @@ $loot = array(
 	array( "id" => "34383", "qual" => "4", "name" => "Kilt of Spiritual Reconstruction" ),
 	array( "id" => "34184", "qual" => "4", "name" => "Brooch of the Highborne" ),
 	array( "id" => "34183", "qual" => "4", "name" => "Shivering Felspine" ),
-	array( "id" => "34185", "qual" => "4", "name" => "Sword Breakers Bulwark" ),
+	array( "id" => "34185", "qual" => "4", "name" => "Sword Breaker's Bulwark" ),
 	array( "id" => "34182", "qual" => "4", "name" => "Grand Magister's Staff of Torrents" ),
 	array( "id" => "34544", "qual" => "4", "name" => "Essence of the Immortals" ),
 	array( "id" => "34856", "qual" => "4", "name" => "Boots of the Forgotten Conqueror" ),
--- games/WoW/bossloot/lootlists/german/loot_fetwins.php
+++ games/WoW/bossloot/lootlists/german/loot_fetwins.php
@@ -1,9 +1,19 @@
 <?php
 $loot = array(
 	array( "id" => "34205", "qual" => "4", "name" => "Shroud of Redeemed Souls" ),
+	array( "id" => "34190", "qual" => "4", "name" => "Crimson Paragon's Cover" ),
+	array( "id" => "34210", "qual" => "4", "name" => "Amice of the Convoker" ),
+	array( "id" => "34209", "qual" => "4", "name" => "Spaulders of Reclamation" ),
+	array( "id" => "34194", "qual" => "4", "name" => "Mantle of the Golden Forest" ),
 	array( "id" => "35290", "qual" => "4", "name" => "Sin'dorei Pendant of Conquest" ),
+	array( "id" => "35291", "qual" => "4", "name" => "Sin'dorei Pendant of Salvation" ),
+	array( "id" => "35292", "qual" => "4", "name" => "Sin'dorei Pendant of Triumph" ),
 	array( "id" => "34189", "qual" => "4", "name" => "Band of Ruinous Delight" ),
+	array( "id" => "34197", "qual" => "4", "name" => "Shiv of Exsanguination" ),
+	array( "id" => "34199", "qual" => "4", "name" => "Archon's Gavel" ),
 	array( "id" => "34203", "qual" => "4", "name" => "Grip of Mannoroth" ),
+	array( "id" => "34198", "qual" => "4", "name" => "Stanchion of Primal Instinct" ),
+	array( "id" => "34196", "qual" => "4", "name" => "Golden Bow of Quel'Thalas" ),
 	array( "id" => "34544", "qual" => "4", "name" => "Essence of the Immortals" ),
 );
 
