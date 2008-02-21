3db450594779bc7f4bcc297abeb35990464d8c9b
sz3 <sz3@eqdkp-plus.eu>
2008-02-21 23:52:53 +0000

    two small fixes

---
+++ games/Vanguard-SOH/lang/english/lang_bossloot.php
@@ -0,0 +1,16 @@
+<?php
+/*********************************************************************
+ *
+ *     EQdkp BossLoot by sz3
+ *
+ *     loot extracted from
+ *         Daviesh' AtlasLoot WoW Addon
+ *
+ *     Licensed under the GNU GPL.  See COPYING for full terms.
+ *
+*********************************************************************/
+
+$lang = array(
+    );
+    
+?>
--- include/bp_htmlfunc.php
+++ include/bp_htmlfunc.php
@@ -88,11 +88,12 @@ global $user;
 function bp_html_get_boss_image_td($bossname, $bosscount) {
   if ($bosscount == 0)
   	$bossname .= "_b";
-  
-  if (file_exists("games/WoW/images/bosses/small/" . $bossname . ".gif")) {
-  	return '<td width="60" height="60" align="center"><img src="games/WoW/images/bosses/small/' . $bossname . '.gif" align="center" border="0" alt="' . $bossname . '" /></td>';
+  $game_arr = explode('_', $eqdkp->config['default_game']);
+
+  if (file_exists("games/'.$game_arr[0].'/images/bosses/small/" . $bossname . ".gif")) {
+  	return '<td width="60" height="60" align="center"><img src="games/'.$game_arr[0].'/images/bosses/small/' . $bossname . '.gif" align="center" border="0" alt="' . $bossname . '" /></td>';
   } else {
-  	return '<td width="60" height="60" align="center"><img src="games/WoW/images/bosses/small/turkey.gif" height="60" border="0" alt= "' . $bossname . '" /></td>';
+  	return '<td width="60" height="60" align="center"><img src="games/'.$game_arr[0].'/images/bosses/small/turkey.gif" height="60" border="0" alt= "' . $bossname . '" /></td>';
   }
 }
 
