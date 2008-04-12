f9906bc8177480febd52e8a2bac5aabe2b08906c
sz3 <sz3@eqdkp-plus.eu>
2008-04-12 17:50:27 +0000

    added chinese language (thanks to jilingshu)

---
+++ games/WoW/lang/chinese/lang_bossbase.php
@@ -0,0 +1,190 @@
+<?php
+
+if ( !defined('EQDKP_INC') ){
+    header('HTTP/1.0 404 Not Found');exit;
+} 
+
+/******Miscellaneous bosses*****/
+$lang['misc'] = array('long' => '其它首领', 'short' =>'其它');
+$lang['azuregos'] = array('long' => '艾索雷葛斯', 'short' => '艾索雷葛斯');
+$lang['kazzak'] = array('long' => '卡扎克', 'short' => '卡扎克');
+
+/******Blackwing Lair*****/
+$lang['bwl'] = array('long' => '黑翼之巢', 'short' => '黑翼');
+$lang['razorgore'] = array('long' => '狂野的拉佐格尔', 'short' => '拉佐格尔');
+$lang['vaelastrasz'] = array('long' => '堕落的瓦拉斯塔兹', 'short' => '瓦拉斯塔兹');
+$lang['lashlayer'] = array('long' => '勒什雷尔', 'short' => '勒什雷尔');
+$lang['firemaw'] = array('long' => '费尔默', 'short' => '费尔默');
+$lang['ebonroc'] = array('long' => '埃博诺克', 'short' => '埃博诺克');
+$lang['flamegor'] = array('long' => '弗莱格尔', 'short' => '弗莱格尔');
+$lang['chromaggus'] = array('long' => '克洛玛古斯', 'short' => '克洛玛古斯');
+$lang['nefarian'] = array('long' => '奈法利安', 'short' => '奈法利安');
+
+/******Onyxia*****/
+$lang['onylair'] = array('long' => '奥妮克希亚的巢穴', 'short' => '奥妮克希亚');
+$lang['onyxia'] = array('long' => '奥妮克希亚', 'short' => '奥妮克希亚');
+
+/******The Emerald Dream*****/
+$lang['dream'] = array('long' => '翡翠梦境', 'short' => '翡翠梦境');
+$lang['ysondre'] = array('long' => '伊森德雷', 'short' => '伊森德雷');
+$lang['taerar'] = array('long' => '泰拉尔', 'short' => '泰拉尔');
+$lang['emeriss'] = array('long' => '艾莫莉丝', 'short' => '艾莫莉丝');
+$lang['lethon'] = array('long' => '莱索恩', 'short' => '莱索恩');
+
+/******Molten Core*****/
+$lang['mc'] = array('long' => '熔火之心', 'short' => '熔火之心');
+$lang['lucifron'] = array('long' => '鲁西弗隆', 'short' => '鲁西弗隆');
+$lang['magmadar'] = array('long' => '玛格曼达', 'short' => '玛格曼达');
+$lang['gehennas'] = array('long' => '基赫纳斯', 'short' => '基赫纳斯');
+$lang['garr'] = array('long' => '加尔', 'short' => '加尔');
+$lang['geddon'] = array('long' => '迦顿男爵', 'short' => '迦顿男爵');
+$lang['shazzrah'] = array('long' => '沙斯拉尔', 'short' => '沙斯拉尔');
+$lang['sulfuron'] = array('long' => '萨弗隆先驱者', 'short' => '先驱者');
+$lang['golemagg'] = array('long' => '焚化者古雷曼格', 'short' => '古雷曼格');
+$lang['majordomo'] = array('long' => '管理者埃克索图斯', 'short' => '埃克索图斯');
+$lang['ragnaros'] = array('long' => '拉格纳罗斯', 'short' => '拉格纳罗斯');
+
+/******Zul'Gurub*****/
+$lang['zg'] = array('long' => '祖尔格拉布', 'short' => '祖尔格拉布');
+$lang['mandokir'] = array('long' => '血领主曼多基尔', 'short' => '曼多基尔');
+$lang['jindo'] = array('long' => '妖术师金度', 'short' => '金度');
+$lang['gahzranka'] = array('long' => '加兹兰卡', 'short' => '加兹兰卡');
+$lang['grilek'] = array('long' => '格里雷克', 'short' => '格里雷克');
+$lang['hazzarah'] = array('long' => '哈扎拉尔', 'short' => '哈扎拉尔');
+$lang['renataki'] = array('long' => '雷纳塔基', 'short' => '雷纳塔基');
+$lang['wushoolay'] = array('long' => '乌苏雷', 'short' => '乌苏雷');
+$lang['thekal'] = array('long' => '高阶祭司塞卡尔', 'short' => '塞卡尔');
+$lang['arlokk'] = array('long' => '高阶祭司娅尔罗', 'short' => '娅尔罗');
+$lang['jeklik'] = array('long' => '高阶祭司耶克里克', 'short' => '耶克里克');
+$lang['marli'] = array('long' => '高阶祭司玛尔里', 'short' => '玛尔里');
+$lang['venoxis'] = array('long' => '高阶祭司温诺希斯', 'short' => '温诺希斯');
+$lang['hakkar'] = array('long' => '哈卡', 'short' => '哈卡');
+
+/******Ruins of Ahn'Qiraj*****/
+$lang['aq20'] = array('long' => '安其拉废墟', 'short' => '废墟');
+$lang['kurinnaxx'] = array('long' => '库林纳克斯', 'short' => '库林纳克斯');
+$lang['rajaxx'] = array('long' => '拉贾克斯将军', 'short' => '拉贾克斯');
+$lang['ayamiss'] = array('long' => '狩猎者阿亚米斯', 'short' => '阿亚米斯');
+$lang['buru'] = array('long' => '吞咽者布鲁', 'short' => '布鲁');
+$lang['moam'] = array('long' => '莫阿姆', 'short' => '莫阿姆');
+$lang['ossirian'] = array('long' => '无疤者奥斯里安', 'short' => '奥斯里安');
+
+/******Gates of Ahn'Qiraj*****/
+$lang['aq40'] = array('long' => '安其拉之门', 'short' => '安其拉');
+$lang['skeram'] = array('long' => '预言者斯克拉姆', 'short' => '斯克拉姆');
+$lang['kri'] = array('long' => '克里勋爵', 'short' => '克里');
+$lang['yauj'] = array('long' => '亚尔基公主', 'short' => '亚尔基');
+$lang['vem'] = array('long' => '维姆', 'short' => '维姆');
+$lang['sartura'] = array('long' => '战争守卫沙尔图拉', 'short' => '沙尔图拉');
+$lang['fankriss'] = array('long' => '顽强的范克瑞斯', 'short' => '范克瑞斯');
+$lang['huhuran'] = array('long' => '哈霍兰公主', 'short' => '哈霍兰');
+$lang['viscidus'] = array('long' => '维希度斯', 'short' => '维希度斯');
+$lang['veknilash'] = array('long' => '维克尼拉斯大帝', 'short' => '维克尼拉斯');
+$lang['veklor'] = array('long' => '维克洛尔大帝', 'short' => '维克洛尔');
+$lang['ouro'] = array('long' => '奥罗', 'short' => '奥罗');
+$lang['cthun'] = array('long' => '克苏恩', 'short' => '克苏恩');
+
+/******Naxxramas*****/
+$lang['naxx'] = array('long' => '纳克萨玛斯', 'short' => '纳克萨玛斯');
+$lang['anubrekhan'] = array('long' => '阿努布雷坎', 'short' => '阿努布雷坎');
+$lang['faerlina'] = array('long' => '黑女巫法琳娜', 'short' => '法琳娜');
+$lang['maexxna'] = array('long' => '迈克斯纳', 'short' => '迈克斯纳');
+$lang['noth'] = array('long' => '瘟疫使者诺斯', 'short' => '诺斯');
+$lang['heigan'] = array('long' => '肮脏的希尔盖', 'short' => '希尔盖');
+$lang['loatheb'] = array('long' => '洛欧塞布', 'short' => '洛欧塞布');
+$lang['patchwerk'] = array('long' => '帕奇维克' , 'short' => '帕奇维克');
+$lang['grobbulus'] = array('long' => '格罗布鲁斯', 'short' => '格罗布鲁斯');
+$lang['gluth'] = array('long' => '格拉斯', 'short' => '格拉斯');
+$lang['thaddius'] = array('long' => '塔迪乌斯', 'short' => '塔迪乌斯');
+$lang['razuvious'] = array('long' => '教官拉苏维奥斯', 'short' => '拉苏维奥斯');
+$lang['gothik'] = array('long' => '收割者戈提克', 'short' => '戈提克');
+$lang['korthazz'] = array('long' => '库尔塔兹领主', 'short' => '库尔塔兹');
+$lang['blaumeux'] = array('long' => '女公爵布劳缪克丝', 'short' => '布劳缪克丝');
+$lang['mograine'] = array('long' => '大领主莫格莱尼', 'short' => '莫格莱尼');
+$lang['zeliek'] = array('long' => '瑟里耶克爵士', 'short' => '瑟里耶克');
+$lang['sapphiron'] = array('long' => '萨菲隆', 'short' => '萨菲隆');
+$lang['kelthuzad'] = array('long' => '克尔苏加德', 'short' => '克尔苏加德');
+
+/******Outland Outdoor Bosses*****/
+$lang['outdoor2'] = array('long' => '外域户外首领', 'short' => '外域');
+$lang['doomkazzak'] = array('long' => '末日领主卡扎克', 'short' => '卡扎克');
+$lang['doomwalker'] = array('long' => '末日行者', 'short' => '末日行者');
+
+/******Magtheridon's Lais******/
+$lang['maglair'] = array('long' => '玛瑟里顿的巢穴', 'short' => '玛瑟里顿');
+$lang['magtheridon'] = array('long' => '玛瑟里顿', 'short' => '玛瑟里顿');
+
+/******Karazhan*****/
+$lang['kara'] = array('long' => '卡拉赞', 'short' => '卡拉赞');
+$lang['attumen'] = array('long' => '猎手阿图门', 'short' => '阿图门');
+$lang['moroes'] = array('long' => '莫罗斯', 'short' => '莫罗斯');
+$lang['maiden'] = array('long' => '贞节圣女', 'short' => '贞节圣女');
+$lang['curator'] = array('long' => '馆长', 'short' => '馆长');
+$lang['illhoof'] = array('long' => '特雷斯坦·邪蹄', 'short' => '特雷斯坦·邪蹄');
+$lang['aran'] = array('long' => '埃兰之影', 'short' => '埃兰');
+$lang['netherspite'] = array('long' => '虚空幽龙', 'short' => '虚空幽龙');
+$lang['malchezaar'] = array('long' => '玛克扎尔王子', 'short' => '玛克扎尔');
+$lang['nightbane'] = array('long' => '夜之魇', 'short' => '夜之魇');
+$lang['chess'] = array('long' => '象棋事件', 'short' => '象棋');
+$lang['opera'] = array('long' => '歌剧院事件', 'short' => '歌剧院');
+
+/******Zul'Aman*****/
+$lang['za'] = array('long' => '祖阿曼', 'short' => '祖阿曼');
+$lang['nalorakk'] = array('long' => 'Nalorakk, Bear Avatar' , 'short' => 'Nalorakk');
+$lang['akilzon'] = array('long' => 'Akil\'Zon, Eagle Avatar', 'short' => 'Akil\'Zon');
+$lang['janalai'] = array('long' => 'Jan\'Alai, Dragonhawk Avatar', 'short' => 'Jan\'Alai');
+$lang['halazzi'] = array('long' => 'Halazzi, Lynx Avatar', 'short' => 'Halazzi');
+$lang['malacrass'] = array('long' => 'Hex Lord Malacrass', 'short' => 'Malacrass');
+$lang['zuljin'] = array('long' => 'Zul\'Jin', 'short' => 'Zul\'Jin');
+
+/******Gruul's Lair*****/
+$lang['gruuls'] = array('long' => '格鲁尔的巢穴', 'short' => '格鲁尔');
+$lang['maulgar'] = array('long' => '加莫尔大王', 'short' => '加莫尔');
+$lang['gruul'] = array('long' => '屠龙者格鲁尔', 'short' => '格鲁尔');
+
+/******Serpentshrine Cavern*****/
+$lang['serpent'] = array('long' => '毒蛇神殿', 'short' => '毒蛇');
+$lang['hydross'] = array('long' => '不稳定的海度斯', 'short' => '海度斯');
+$lang['leotheras'] = array('long' => '盲眼者莱欧瑟拉斯', 'short' => '莱欧瑟拉斯');
+$lang['karathress'] = array('long' => '深水领主卡拉瑟雷斯', 'short' => '卡拉瑟雷斯');
+$lang['morogrim'] = array('long' => '莫洛格里·踏潮者', 'short' => '莫洛格里');
+$lang['lurker'] = array('long' => '鱼斯拉', 'short' => '鱼斯拉');
+$lang['vashj'] = array('long' => '瓦丝琪', 'short' => '瓦丝琪');
+
+/******The Eye*****/
+$lang['eye'] = array('long' => '风暴要塞', 'short' => '风暴');
+$lang['alar'] = array('long' => '凤凰神奥', 'short' => '奥');
+$lang['vreaver'] = array('long' => '空灵机甲', 'short' => '空灵机甲');
+$lang['solarian'] = array('long' => '大星术师索兰莉安', 'short' => '索兰莉安');
+$lang['kaelthas'] = array('long' => '凯尔萨斯·逐日者', 'short' => '凯尔萨斯');
+
+/******Battle of Mount Hyjal*****/
+$lang['hyjal'] = array('long' => '海加尔峰', 'short' => '海加尔');
+$lang['winterchill'] = array('long' => '雷基·冬寒', 'short' => '冬寒');
+$lang['anetheron'] = array('long' => '安纳塞隆', 'short' => '安纳塞隆');
+$lang['kazrogal'] = array('long' => '卡兹洛加', 'short' => '卡兹洛加');
+$lang['azgalor'] = array('long' => '阿兹加洛', 'short' => '阿兹加洛');
+$lang['archimonde'] = array('long' => '阿克蒙德', 'short' => '阿克蒙德');
+
+/******The Black Temple*****/
+$lang['temple'] = array('long' => '黑暗神殿', 'short' => '神殿');
+$lang['najentus'] = array('long' => '高阶督军纳因图斯', 'short' => '纳因图斯');
+$lang['supremus'] = array('long' => '苏普雷姆斯', 'short' => '苏普雷姆斯');
+$lang['akama'] = array('long' => '阿卡玛', 'short' => '阿卡玛');
+$lang['gorefiend'] = array('long' => '塔隆·血魔', 'short' => '血魔');
+$lang['essence'] = array('long' => '灵魂之匣', 'short' => '灵魂之匣');
+$lang['bloodboil'] = array('long' => ' 	古尔图格·血沸', 'short' => '血沸');
+$lang['shahraz'] = array('long' => '莎赫拉丝主母', 'short' => '莎赫拉丝');
+$lang['council'] = array('long' => '伊利达雷议会', 'short' => '伊利达雷');
+$lang['illidan'] = array('long' => '伊利丹·怒风', 'short' => '伊利丹');
+
+/******Sunwell Plateau*****/
+$lang['sunwell'] = array('long' => '太阳井高地', 'short' =>'太阳井');
+$lang['kalecgos'] = array('long' => 'Kalecgos', 'short' => 'Kalecgos');
+$lang['brutallus'] = array('long' => 'Brutallus', 'short' => 'Brutallus');
+$lang['felmyst'] = array('long' => 'Felmyst', 'short' => 'Felmyst');
+$lang['fetwins'] = array('long' => 'Alythess & Sacrolash', 'short' => 'Twins');
+$lang['muru'] = array('long' => 'M\'uru', 'short' => 'M\'uru');
+$lang['kiljaeden'] = array('long' => '基尔加丹', 'short' => '基尔加丹');
+
+?>
---
+++ games/WoW/lang/chinese/lang_bossloot.php
@@ -0,0 +1,28 @@
+<?php
+/*********************************************************************
+ *
+ *     EQdkp BossLoot by sz3
+ *
+ *     loot extracted from
+ *         Daviesh' AtlasLoot WoW Addon
+ *
+ *     Licensed under the GNU GPL.  See COPYING for full terms.
+ *
+*********************************************************************/
+
+$lang = array(
+    //Item languages
+    'item_lang_english' => '英语',
+    'item_lang_chinese' => '中文',
+    'item_lang_german' => '德语',
+
+    //Item qualities
+    'item_qual_0' => '粗糙',
+    'item_qual_1' => '标准',
+    'item_qual_2' => '稀有',
+    'item_qual_3' => '精良',
+    'item_qual_4' => '史诗',
+    'item_qual_5' => '传奇'
+    );
+    
+?>
