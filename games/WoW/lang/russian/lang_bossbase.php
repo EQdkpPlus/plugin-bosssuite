a4fc1f247451c24fc2c7801b1ab67c7449088de6
sz3 <sz3@eqdkp-plus.eu>
2008-06-18 22:56:18 +0000

    bs: russian wow lang files

---
+++ games/WoW/lang/russian/lang_bossbase.php
@@ -0,0 +1,197 @@
+<?php
+/******************************
+ * EQdkp BossSuite
+ * (c) 2006 - 2008
+ * created by sz3
+ * ---------------------------
+ * $Id: lang_bossbase.php 1943 2008-04-24 23:37:28Z sz3 $
+ ******************************/
+
+if ( !defined('EQDKP_INC') ){
+    header('HTTP/1.0 404 Not Found');exit;
+}
+
+/******Miscellaneous bosses*****/
+$lang['misc'] = array('long' => 'Miscellaneous bosses', 'short' => 'Miscellaneous bosses');
+$lang['azuregos'] = array('long' => 'Азурегос', 'short' => 'Azuregos');
+$lang['kazzak'] = array('long' => 'Лорд Каззак', 'short' => 'Каззак');
+
+/******Логово Чернокрыла*****/
+$lang['bwl'] = array('long' => 'Логово Чернокрыла', 'short' => 'Логово Чернокрыла');
+$lang['razorgore'] = array('long' => 'Razorgore the Untamed', 'short' => 'Razorgore');
+$lang['vaelastrasz'] = array('long' => 'Vaelastrasz the Corrupt', 'short' => 'Vaelastrasz');
+$lang['lashlayer'] = array('long' => 'Кровавый Лорд Lashlayer', 'short' => 'Lashlayer');
+$lang['firemaw'] = array('long' => 'Firemaw', 'short' => 'Firemaw');
+$lang['ebonroc'] = array('long' => 'Ebonroc', 'short' => 'Ebonroc');
+$lang['flamegor'] = array('long' => 'Flamegor', 'short' => 'Flamegor');
+$lang['chromaggus'] = array('long' => 'Chromaggus', 'short' => 'Chromaggus');
+$lang['nefarian'] = array('long' => 'Нефарион', 'short' => 'Нефарион');
+
+/******Ониксия*****/
+$lang['onylair'] = array('long' => 'Логово Ониксии', 'short' => 'Ониксия');
+$lang['onyxia'] = array('long' => 'Ониксия', 'short' => 'Ониксия');
+
+/******Изумрудный Сон*****/
+$lang['dream'] = array('long' => 'Изумрудный Сон', 'short' => 'Изумрудный Сон');
+$lang['ysondre'] = array('long' => 'Ysondre', 'short' => 'Ysondre');
+$lang['taerar'] = array('long' => 'Taerar', 'short' => 'Taerar');
+$lang['emeriss'] = array('long' => 'Emeriss', 'short' => 'Emeriss');
+$lang['lethon'] = array('long' => 'Lethon', 'short' => 'Lethon');
+
+/******Расправленное ядро*****/
+$lang['mc'] = array('long' => 'Расправленное ядро', 'short' => 'Расправленное ядро');
+$lang['lucifron'] = array('long' => 'Люцифрон', 'short' => 'Люцифрон');
+$lang['magmadar'] = array('long' => 'Magmadar', 'short' => 'Magmadar');
+$lang['gehennas'] = array('long' => 'Gehennas', 'short' => 'Gehennas');
+$lang['garr'] = array('long' => 'Garr', 'short' => 'Garr');
+$lang['geddon'] = array('long' => 'Барон Геддон', 'short' => 'Геддон');
+$lang['shazzrah'] = array('long' => 'Shazzrah', 'short' => 'Shazzrah');
+$lang['sulfuron'] = array('long' => 'Sulfuron Harbringer', 'short' => 'Sulfuron');
+$lang['golemagg'] = array('long' => 'Golemagg the Incinerator', 'short' => 'Golemagg');
+$lang['majordomo'] = array('long' => 'Majordomo Executus', 'short' => 'Majordomo');
+$lang['ragnaros'] = array('long' => 'Рагнарос', 'short' => 'Рагнарос');
+
+/******Zul'Gurub*****/
+$lang['zg'] = array('long' => 'Зул-Гуруб', 'short' => 'Зул-Гуруб');
+$lang['mandokir'] = array('long' => 'Bloodlord Mandokir', 'short' => 'Mandokir');
+$lang['jindo'] = array('long' => 'Jin\'do the Hexxer', 'short' => 'Jin\'do');
+$lang['gahzranka'] = array('long' => 'Gahz\'ranka', 'short' => 'Gahz\'ranka');
+$lang['grilek'] = array('long' => 'Gri\'lek', 'short' => 'Gri\'lek');
+$lang['hazzarah'] = array('long' => 'Hazza\'rah', 'short' => 'Hazza\'rah');
+$lang['renataki'] = array('long' => 'Renataki', 'short' => 'Renataki');
+$lang['wushoolay'] = array('long' => 'Wushoolay', 'short' => 'Wushoolay');
+$lang['thekal'] = array('long' => 'Высший жрец Thekal', 'short' => 'Thekal');
+$lang['arlokk'] = array('long' => 'Высшая жрица Arlokk', 'short' => 'Arlokk');
+$lang['jeklik'] = array('long' => 'Высшая жрица Jeklik', 'short' => 'Jeklik');
+$lang['marli'] = array('long' => 'Высшая жрица Mar\'li', 'short' => 'Mar\'li');
+$lang['venoxis'] = array('long' => 'Высший жрец Веноксис', 'short' => 'Веноксис');
+$lang['hakkar'] = array('long' => 'Хаккар', 'short' => 'Хаккар');
+
+/******Руины Ahn\'Qiraj*****/
+$lang['aq20'] = array('long' => 'Руины Ahn\'Qiraj', 'short' => 'Руины Ahn\'Qiraj');
+$lang['kurinnaxx'] = array('long' => 'Kurinnaxx', 'short' => 'Kurinnaxx');
+$lang['rajaxx'] = array('long' => 'Генерал Rajaxx', 'short' => 'Rajaxx');
+$lang['ayamiss'] = array('long' => 'Ayamiss Охотник', 'short' => 'Ayamiss');
+$lang['buru'] = array('long' => 'Буру Gorger', 'short' => 'Буру');
+$lang['moam'] = array('long' => 'Moam', 'short' => 'Moam');
+$lang['ossirian'] = array('long' => 'Ossirian the Unscarred', 'short' => 'Ossirian');
+
+/******Врата Ahn\'Qiraj*****/
+$lang['aq40'] = array('long' => 'Врата Ahn\'Qiraj', 'short' => 'Врата Ahn\'Qiraj');
+$lang['skeram'] = array('long' => 'The Prophet Skeram', 'short' => 'Skeram');
+$lang['kri'] = array('long' => 'Лорд Kri', 'short' => 'Lord Kri');
+$lang['yauj'] = array('long' => 'Принцесса Yauj', 'short' => 'Принцесса Yauj');
+$lang['vem'] = array('long' => 'Вим', 'short' => 'Вим');
+$lang['sartura'] = array('long' => 'Battleguard Sartura', 'short' => 'Sartura');
+$lang['fankriss'] = array('long' => 'Fankriss the Unyielding', 'short' => 'Fankriss');
+$lang['huhuran'] = array('long' => 'Принцесса Huhuran', 'short' => 'Huhuran');
+$lang['viscidus'] = array('long' => 'Viscidus', 'short' => 'Viscidus');
+$lang['veknilash'] = array('long' => 'Emperor Vek\'nilash', 'short' => 'Vek\'nilash');
+$lang['veklor'] = array('long' => 'Emperor Vek\'lor', 'short' => 'Vek\'lor');
+$lang['ouro'] = array('long' => 'Ouro', 'short' => 'Ouro');
+$lang['cthun'] = array('long' => 'C\'Thun', 'short' => 'C\'Thun');
+
+/******Naxxramas*****/
+$lang['naxx'] = array('long' => 'Naxxramas', 'short' => 'Naxxramas');
+$lang['anubrekhan'] = array('long' => 'Anub\'Rekhan', 'short' => 'Anub\'Rekhan');
+$lang['faerlina'] = array('long' => 'Grand Widow Faerlina', 'short' => 'Faerlina');
+$lang['maexxna'] = array('long' => 'Maexxna', 'short' => 'Maexxna');
+$lang['noth'] = array('long' => 'Noth the Plaguebringer', 'short' => 'Noth');
+$lang['heigan'] = array('long' => 'Heigan the Unclean', 'short' => 'Heigan');
+$lang['loatheb'] = array('long' => 'Loatheb', 'short' => 'Loatheb');
+$lang['patchwerk'] = array('long' => 'Patchwerk' , 'short' => 'Patchwerk');
+$lang['grobbulus'] = array('long' => 'Grobbulus', 'short' => 'Grobbulus');
+$lang['gluth'] = array('long' => 'Gluth', 'short' => 'Gluth');
+$lang['thaddius'] = array('long' => 'Thaddius', 'short' => 'Thaddius');
+$lang['razuvious'] = array('long' => 'Instructor Razuvious', 'short' => 'Razuvious');
+$lang['gothik'] = array('long' => 'Gothik the Harvester', 'short' => 'Gothik');
+$lang['korthazz'] = array('long' => 'Thane Korth\'azz', 'short' => 'Korth\'azz');
+$lang['blaumeux'] = array('long' => 'Леди Blaumeux', 'short' => 'Blaumeux');
+$lang['mograine'] = array('long' => 'Верховный Лорд Mograine', 'short' => 'Mograine');
+$lang['zeliek'] = array('long' => 'Sir Zeliek', 'short' => 'Sir Zeliek');
+$lang['sapphiron'] = array('long' => 'Sapphiron', 'short' => 'Sapphiron');
+$lang['kelthuzad'] = array('long' => 'Кел-Тузед, Верховный Маг', 'short' => 'Кел-Тузед');
+
+/******Outland Outdoor Bosses*****/
+$lang['outdoor2'] = array('long' => 'Outland Outdoor Bosses', 'short' => 'Outland Outdoor Bosses');
+$lang['doomkazzak'] = array('long' => 'Роковой Лорд Каззак', 'short' => 'Роковой Каззак');
+$lang['doomwalker'] = array('long' => 'Doomwalker', 'short' => 'Doomwalker');
+
+/******Логово Магтеридона******/
+$lang['maglair'] = array('long' => 'Логово Магтеридона', 'short' => 'Магтеридон');
+$lang['magtheridon'] = array('long' => 'Магтеридон', 'short' => 'Магтеридон');
+
+/******Каражан*****/
+$lang['kara'] = array('long' => 'Каражан', 'short' => 'Каражан');
+$lang['attumen'] = array('long' => 'Attumen the Huntsman', 'short' => 'Attumen');
+$lang['moroes'] = array('long' => 'Мороес', 'short' => 'Мороес');
+$lang['maiden'] = array('long' => 'Дева ', 'short' => 'Дева');
+$lang['curator'] = array('long' => 'Куратор', 'short' => 'Куратор');
+$lang['illhoof'] = array('long' => 'Terestian Illhoof', 'short' => 'Illhoof');
+$lang['aran'] = array('long' => 'Аран', 'short' => 'Аран');
+$lang['netherspite'] = array('long' => 'Netherspite', 'short' => 'Netherspite');
+$lang['malchezaar'] = array('long' => 'Принц Мальчезар', 'short' => 'Мальчезар');
+$lang['nightbane'] = array('long' => 'Nightbane', 'short' => 'Nightbane');
+$lang['chess'] = array('long' => 'Шахматы', 'short' => 'Шахматы');
+$lang['opera'] = array('long' => 'Опера', 'short' => 'Опера');
+
+/******Зул-Аман*****/
+$lang['za'] = array('long' => 'Зул-Аман', 'short' => 'Зул-Аман');
+$lang['nalorakk'] = array('long' => 'Nalorakk,Медведь' , 'short' => 'Nalorakk');
+$lang['akilzon'] = array('long' => 'Akil\'Zon, Орел', 'short' => 'Akil\'Zon');
+$lang['janalai'] = array('long' => 'Jan\'Alai, Драконо-Ястреб', 'short' => 'Jan\'Alai');
+$lang['halazzi'] = array('long' => 'Halazzi, Рысь', 'short' => 'Halazzi');
+$lang['malacrass'] = array('long' => 'Hex Lord Malacrass', 'short' => 'Malacrass');
+$lang['zuljin'] = array('long' => 'Зул-джин', 'short' => 'Зул-джин');
+
+/******Логово Грууля*****/
+$lang['gruuls'] = array('long' => 'Логово Грууля', 'short' => 'Логово Грууля');
+$lang['maulgar'] = array('long' => 'Верховный Колорь Маулгар', 'short' => 'Маулгар');
+$lang['gruul'] = array('long' => 'Грууль, Убийца Драконов', 'short' => 'Грууль');
+
+/******Пещера Святой Змеи*****/
+$lang['serpent'] = array('long' => 'Пещера Святой Змеи', 'short' => 'Пещера Святой Змеи');
+$lang['hydross'] = array('long' => 'Гидросс Непостоянный', 'short' => 'Гидросс');
+$lang['leotheras'] = array('long' => 'Leotheras the Blind', 'short' => 'Leotheras');
+$lang['karathress'] = array('long' => 'Фантом-Лорд Каратресс', 'short' => 'Каратресс');
+$lang['morogrim'] = array('long' => 'Morogrim Tidewalker', 'short' => 'Morogrim');
+$lang['lurker'] = array('long' => 'The Lurker Below', 'short' => 'Lurker');
+$lang['vashj'] = array('long' => 'Леди Вайши, Королева змей', 'short' => 'Вайши');
+
+/******Крепость Бури: Око*****/
+$lang['eye'] = array('long' => 'Крепость Бури: Око', 'short' => 'Око');
+$lang['alar'] = array('long' => 'Al\'Ar, Феникс бога', 'short' => 'Al\'Ar');
+$lang['vreaver'] = array('long' => 'Void Reaver', 'short' => 'Reaver');
+$lang['solarian'] = array('long' => 'High Astromancer Solarian', 'short' => 'Solarian');
+$lang['kaelthas'] = array('long' => 'Кель, Чародей Крови', 'short' => 'Кель');
+
+/******Битва у горы Хиджаль*****/
+$lang['hyjal'] = array('long' => 'Битва у горы Хиджаль', 'short' => 'Битва у горы Хиджаль');
+$lang['winterchill'] = array('long' => 'Rage Winterchill', 'short' => 'Winterchill');
+$lang['anetheron'] = array('long' => 'Anetheron', 'short' => 'Anetheron');
+$lang['kazrogal'] = array('long' => 'Kaz\'rogal', 'short' => 'Kaz\'rogal');
+$lang['azgalor'] = array('long' => 'Азгалор', 'short' => 'Азгалор');
+$lang['archimonde'] = array('long' => 'Архимонд', 'short' => 'Архимонд');
+
+/******Храм Тьмы*****/
+$lang['temple'] = array('long' => 'Храм Тьмы', 'short' => 'Храм Тьмы');
+$lang['najentus'] = array('long' => 'High Warlord Naj\'entus', 'short' => 'Naj\'entus');
+$lang['supremus'] = array('long' => 'Супремус', 'short' => 'Супремус');
+$lang['akama'] = array('long' => 'Акама', 'short' => 'Акама');
+$lang['gorefiend'] = array('long' => 'Teron Gorefiend', 'short' => 'Gorefiend');
+$lang['essence'] = array('long' => 'Essence of Souls', 'short' => 'Essence');
+$lang['bloodboil'] = array('long' => 'Gurtogg Bloodboil', 'short' => 'Bloodboil');
+$lang['shahraz'] = array('long' => 'Мать Shahraz', 'short' => 'Shahraz');
+$lang['council'] = array('long' => 'Illidari Council', 'short' => 'Council');
+$lang['illidan'] = array('long' => 'Иллидан Свирепый', 'short' => 'Иллидан');
+
+/******Солнечный Источник*****/
+$lang['sunwell'] = array('long' => 'Солнечный Источник', 'short' => 'Солнечный Источник');
+$lang['kalecgos'] = array('long' => 'Kalecgos', 'short' => 'Kalecgos');
+$lang['brutallus'] = array('long' => 'Brutallus', 'short' => 'Brutallus');
+$lang['felmyst'] = array('long' => 'Felmyst', 'short' => 'Felmyst');
+$lang['fetwins'] = array('long' => 'Alythess & Sacrolash', 'short' => 'Twins');
+$lang['muru'] = array('long' => 'М\'уру', 'short' => 'М\'уру');
+$lang['kiljaeden'] = array('long' => 'Кил-Джеден Обманщик', 'short' => 'Кил-Джеден');
+
+?>
---
+++ games/WoW/lang/russian/lang_bossloot.php
@@ -0,0 +1,28 @@
+<?php
+/*********************************************************************
+ *
+ *     EQdkp BossLoot by sz3
+ *
+ *     loot extracted from
+ *         Daviesh' AtlasLoot WoW Addon
+ *
+ *     Licensed under the GNU GPL.  See COPYING for full terms.
+ *
+*********************************************************************/
+
+$lang = array(
+    //Item languages
+    'item_lang_english' => 'Английский',
+    'item_lang_german' => 'Немецкий',
+    'item_lang_russian' => 'Русский',
+
+    //Item qualities
+    'item_qual_0' => 'Плохое',
+    'item_qual_1' => 'Необычное',
+    'item_qual_2' => 'Обычное',
+    'item_qual_3' => 'Редкое',
+    'item_qual_4' => 'Эпическое',
+    'item_qual_5' => 'Легендарное'
+    );
+
+?>
