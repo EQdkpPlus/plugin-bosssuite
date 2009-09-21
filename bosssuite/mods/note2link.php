c15864c873145db2da46d1ef5995ad5b3e93860a
sz3 <sz3@eqdkp-plus.eu>
2009-09-21 21:57:57 +0000

    fix listraids bug if game is not supported, fixed ndl list bug if game == wow, bump to 4.6.0 ready for open beta.

--- bosssuite/mods/note2link.php
+++ bosssuite/mods/note2link.php
@@ -23,89 +23,96 @@ if (!defined('EQDKP_INC')) {
 // new mgs class
 require_once(dirname(__FILE__).'/../include/bsmgs.class.php');
 $mybsmgs = new BSMGS();
-$mybsmgs->load_game_specific_language('bossbase');
-
-// sql class
-require_once(dirname(__FILE__).'/../include/bssql.class.php');
-$mybssql = new BSSQL();
-
-//get config
-$bb_conf = $mybssql->get_config('bossbase');
-$bb_delim = array (
-  'rnote' => '/'.$bb_conf['noteDelim'].'/',
-	'rname' => '/'.$bb_conf['nameDelim'].'/'
-);
-
-$bossInfo = $bb_conf['bossInfo'];
-$zoneInfo = $bb_conf['zoneInfo'];
-$bb_pboss = $mybssql->get_parse_boss();
-$bb_pzone = $mybssql->get_parse_zone();
-$bzone = $mybssql->get_bzone();
-
-function bl_note2link($rnote, $rname=""){
-global $mybsmgs, $bb_conf, $mybssql, $bossInfo, $zoneInfo, $bb_pboss, $bb_pzone, $bb_delim, $bzone, $eqdkp_root_path, $SID;  
-
-	if ($rnote == '')
-		return $rnote;
-  
+if (!$mybsmgs->game_supported('bossbase')){
+  function bl_note2link($raidnote,$raidname=''){
+    return $raidnote;
+  }
+}else{
   if (!$mybsmgs->game_supported('bossbase')){
-    return $rnote;
+      message_die("GAME NOT SUPPORTED");
   }
-
-	if($bb_conf['depmatch']){
-  	if ($bb_delim[$zoneInfo] != "//"){
-  		$zone_element = preg_split($bb_delim[$zoneInfo], $rname, -1, PREG_SPLIT_NO_EMPTY);
-  	} else {
-  		$zone_element = array($rname);
-  	}
-  	
-    $sbzone = array();
-  	foreach ($zone_element as $raid){
-  	  foreach($bzone as $zone => $bosses){
-  	    $zparseList = preg_split("/\',[ ]*\'/", stripslashes(trim($bb_pzone['pz_'.$zone], "\' ")));
-  	    if ($mybssql->in_array_nocase(stripslashes(trim($raid)), $zparseList)) {
-  	      $sbzone[$zone] = $bosses;
-  	    }	
-  	  }
+  // sql class
+  require_once(dirname(__FILE__).'/../include/bssql.class.php');
+  $mybssql = new BSSQL();
+  
+  //get config
+  $bb_conf = $mybssql->get_config('bossbase');
+  $bb_delim = array (
+    'rnote' => '/'.$bb_conf['noteDelim'].'/',
+  	'rname' => '/'.$bb_conf['nameDelim'].'/'
+  );
+  
+  $bossInfo = $bb_conf['bossInfo'];
+  $zoneInfo = $bb_conf['zoneInfo'];
+  $bb_pboss = $mybssql->get_parse_boss();
+  $bb_pzone = $mybssql->get_parse_zone();
+  $bzone = $mybssql->get_bzone();
+  
+  function bl_note2link($rnote, $rname=""){
+  global $mybsmgs, $bb_conf, $mybssql, $bossInfo, $zoneInfo, $bb_pboss, $bb_pzone, $bb_delim, $bzone, $eqdkp_root_path, $SID;  
+  
+  	if ($rnote == '')
+  		return $rnote;
+    
+    if (!$mybsmgs->game_supported('bossbase')){
+      return $rnote;
     }
-  }else{
-    $sbzone = $bzone;
-  }
-
-	if ($bb_delim[$bossInfo] != "//"){
-		$boss_element = preg_split($bb_delim[$bossInfo], $rnote, -1, PREG_SPLIT_NO_EMPTY);
-	} else {
-		$boss_element = array($rnote);
-	}
-  $count = sizeof($boss_element);
   
-  if($count == 0)
-    return $rnote;
+  	if($bb_conf['depmatch']){
+    	if ($bb_delim[$zoneInfo] != "//"){
+    		$zone_element = preg_split($bb_delim[$zoneInfo], $rname, -1, PREG_SPLIT_NO_EMPTY);
+    	} else {
+    		$zone_element = array($rname);
+    	}
+    	
+      $sbzone = array();
+    	foreach ($zone_element as $raid){
+    	  foreach($bzone as $zone => $bosses){
+    	    $zparseList = preg_split("/\',[ ]*\'/", stripslashes(trim($bb_pzone['pz_'.$zone], "\' ")));
+    	    if ($mybssql->in_array_nocase(stripslashes(trim($raid)), $zparseList)) {
+    	      $sbzone[$zone] = $bosses;
+    	    }	
+    	  }
+      }
+    }else{
+      $sbzone = $bzone;
+    }
   
-	foreach ($boss_element as $raid){
-    foreach ($sbzone as $zone => $bosses){
-		  $boss_found = $false;
-      foreach ($bosses as $boss){
-      	$bparseList = preg_split("/\',[ ]*\'/", stripslashes(trim($bb_pboss['pb_'.$boss], "\' ")));
-  			if ($mybssql->in_array_nocase(stripslashes(trim($raid)), $bparseList)) {
-          $bl = '<a href="'.$eqdkp_root_path.'plugins/bosssuite/bossloot.php'.$SID.'&amp;bossid='.$boss.'">'.$raid.'</a>';
-          if($count == 1){
-            $rnote = str_replace(trim($raid), $bl, $rnote);
-          }else{
-            $matchme = "/(".$bb_conf['noteDelim'].")(".preg_quote($raid).")/";
-            $rnote = preg_replace($matchme, "\\1$bl", $rnote);
-            
-            $matchme = "/(".preg_quote($raid).")(".$bb_conf['noteDelim'].")/";
-            $rnote = preg_replace($matchme, "$bl\\2", $rnote);
-          }
-          $boss_found = true;
+  	if ($bb_delim[$bossInfo] != "//"){
+  		$boss_element = preg_split($bb_delim[$bossInfo], $rnote, -1, PREG_SPLIT_NO_EMPTY);
+  	} else {
+  		$boss_element = array($rnote);
+  	}
+    $count = sizeof($boss_element);
+    
+    if($count == 0)
+      return $rnote;
+    
+  	foreach ($boss_element as $raid){
+      foreach ($sbzone as $zone => $bosses){
+  		  $boss_found = $false;
+        foreach ($bosses as $boss){
+        	$bparseList = preg_split("/\',[ ]*\'/", stripslashes(trim($bb_pboss['pb_'.$boss], "\' ")));
+    			if ($mybssql->in_array_nocase(stripslashes(trim($raid)), $bparseList)) {
+            $bl = '<a href="'.$eqdkp_root_path.'plugins/bosssuite/bossloot.php'.$SID.'&amp;bossid='.$boss.'">'.$raid.'</a>';
+            if($count == 1){
+              $rnote = str_replace(trim($raid), $bl, $rnote);
+            }else{
+              $matchme = "/(".$bb_conf['noteDelim'].")(".preg_quote($raid).")/";
+              $rnote = preg_replace($matchme, "\\1$bl", $rnote);
+              
+              $matchme = "/(".preg_quote($raid).")(".$bb_conf['noteDelim'].")/";
+              $rnote = preg_replace($matchme, "$bl\\2", $rnote);
+            }
+            $boss_found = true;
+            break;
+    			}		
+    		}//end for bosses
+    	  if($boss_found)
           break;
-  			}		
-  		}//end for bosses
-  	  if($boss_found)
-        break;
-    }//end for zones
+      }//end for zones
+    }
+    return $rnote;	
   }
-  return $rnote;	
 }
 ?>
